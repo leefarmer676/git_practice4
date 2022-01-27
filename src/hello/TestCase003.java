5bba680efddaaf5279b8aac7b1ff1b7e41ae8b35
leefarmer676 <lee.farmer0208@gmail.com>
2022-01-27 08:30:36 -0700

    creating conflict in tc003 in master

---
+++ src/hello/TestCase003.java
@@ -0,0 +1,8 @@
+package hello;
+
+public class TestCase003 {
+
+    //this is unknown changes to feature branch tha texitss in master; now git will not know what
+    //to do with this
+    //when wemerge and conflict will hapen
+}
